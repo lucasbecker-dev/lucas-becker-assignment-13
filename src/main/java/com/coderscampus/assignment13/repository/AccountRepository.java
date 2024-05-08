69ec82b77d4e7eb285bcf55a070f4ca522cdeab9
Lucas Becker <lucasbecker.dev@gmail.com>
2024-05-08 13:00:04 -0400

    initialized project files

---
+++ src/main/java/com/coderscampus/assignment13/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.coderscampus.assignment13.repository;
+
+import com.coderscampus.assignment13.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
