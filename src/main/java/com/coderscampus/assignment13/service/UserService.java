5594d9d35285a474d7d29656f6df1fdc2bd20000
Lucas Becker <lucasbecker.dev@gmail.com>
2024-05-09 11:30:08 -0400

    comment out account creation in user service (for now)

--- src/main/java/com/coderscampus/assignment13/service/UserService.java
+++ src/main/java/com/coderscampus/assignment13/service/UserService.java
@@ -1,6 +1,5 @@
 package com.coderscampus.assignment13.service;
 
-import com.coderscampus.assignment13.domain.Account;
 import com.coderscampus.assignment13.domain.User;
 import com.coderscampus.assignment13.repository.AccountRepository;
 import com.coderscampus.assignment13.repository.UserRepository;
@@ -50,19 +49,19 @@ public class UserService {
     }
 
     public User saveUser(User user) {
-        if (user.getUserId() == null) {
-            Account checking = new Account();
-            checking.setAccountName("Checking Account");
-            checking.getUsers().add(user);
-            Account savings = new Account();
-            savings.setAccountName("Savings Account");
-            savings.getUsers().add(user);
-
-            user.getAccounts().add(checking);
-            user.getAccounts().add(savings);
-            accountRepo.save(checking);
-            accountRepo.save(savings);
-        }
+//        if (user.getUserId() == null) {
+//            Account checking = new Account();
+//            checking.setAccountName("Checking Account");
+//            checking.getUsers().add(user);
+//            Account savings = new Account();
+//            savings.setAccountName("Savings Account");
+//            savings.getUsers().add(user);
+//
+//            user.getAccounts().add(checking);
+//            user.getAccounts().add(savings);
+//            accountRepo.save(checking);
+//            accountRepo.save(savings);
+//        }
         return userRepo.save(user);
     }
 
