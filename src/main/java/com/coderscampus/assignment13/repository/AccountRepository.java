6e6a06f2511f4696a551d86a2149fe3097aa32bc
Lucas Becker <lucasbecker.dev@gmail.com>
2024-05-10 12:04:27 -0400

    added custom query for practice

--- src/main/java/com/coderscampus/assignment13/repository/AccountRepository.java
+++ src/main/java/com/coderscampus/assignment13/repository/AccountRepository.java
@@ -3,12 +3,15 @@ package com.coderscampus.assignment13.repository;
 import com.coderscampus.assignment13.domain.Account;
 import com.coderscampus.assignment13.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    @Query("select a from Account a where accountId = :accountId")
     Account findByAccountId(Long accountId);
 
     List<Account> findByUsers(User user);
