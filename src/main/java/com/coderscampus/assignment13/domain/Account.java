f5210e6edba207d88dbe8378fb79a622f77e991b
Lucas Becker <lucasbecker.dev@gmail.com>
2024-05-20 10:49:56 -0400

    refactored to use Lombok

--- src/main/java/com/coderscampus/assignment13/domain/Account.java
+++ src/main/java/com/coderscampus/assignment13/domain/Account.java
@@ -1,51 +1,22 @@
 package com.coderscampus.assignment13.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "accounts")
+@Data
 public class Account {
-    private Long accountId;
-    private String accountName;
-    private Set<Transaction> transactions = new HashSet<>();
-    private Set<User> users = new HashSet<>();
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(Long accountId) {
-        this.accountId = accountId;
-    }
-
+    private Long accountId;
     @Column(length = 100)
-    public String getAccountName() {
-        return accountName;
-    }
-
-    public void setAccountName(String accountName) {
-        this.accountName = accountName;
-    }
-
+    private String accountName;
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
-    public Set<Transaction> getTransactions() {
-        return transactions;
-    }
-
-    public void setTransactions(Set<Transaction> transactions) {
-        this.transactions = transactions;
-    }
-
+    private Set<Transaction> transactions = new HashSet<>();
     @ManyToMany(mappedBy = "accounts")
-    public Set<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(Set<User> users) {
-        this.users = users;
-    }
+    private Set<User> users = new HashSet<>();
 }
